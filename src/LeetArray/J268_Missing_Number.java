921654fff9106d1426091c5255a3fa9ef60cb089
WaltXin <xiongx@tcd.ie>
2017-04-26 19:10:28 +0100

    Use summary

---
+++ src/LeetArray/J268_Missing_Number.java
@@ -0,0 +1,22 @@
+package LeetArray;
+
+public class J268_Missing_Number {
+
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 5, 3, 6, 0};
+		System.out.println(solution(arr));
+	}
+	
+	public static int solution(int[] nums) {
+		int n = nums.length + 1;
+		//0 - n summary  [0, n] sn = n * (al + an) / 2 --> sn = n * an / 2 because a1 = 0 && an = n - 1
+		int summary = n * (n - 1) / 2;
+		int arrSum = 0;
+		for (int i : nums) {
+			arrSum += i;
+		}
+		return summary - arrSum;
+		
+	}
+
+}
