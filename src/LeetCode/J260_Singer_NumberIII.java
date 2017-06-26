4e3d4b0050fb3c4a2b3f8e8c6614ac4b3981955b
WaltXin <xiongx@tcd.ie>
2017-06-26 20:41:21 +0100

    last bit set a&-a

---
+++ src/LeetCode/J260_Singer_NumberIII.java
@@ -0,0 +1,35 @@
+package LeetCode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Created by walt on 26/06/2017.
+ */
+public class J260_Singer_NumberIII {
+    public int[] singleNumber(int[] nums) {
+        int diff = 0;
+        for (int num : nums) {
+            diff ^= num;
+        }
+        //get last set bit(get last 1)
+        diff &= -diff;
+        int[] result = new int[2];
+        for (int num : nums) {
+            if ((num & diff) == 0) {
+                //the bit is not set
+                result[0] ^= num;
+            } else {
+                //the bit is set
+                result[1] ^= num;
+            }
+        }
+        return result;
+    }
+    @Test
+    public void test() {
+        int[] test1  = {2,2,1,3};
+        assertArrayEquals(new int[]{1,3},new J260_Singer_NumberIII().singleNumber(test1));
+    }
+}
