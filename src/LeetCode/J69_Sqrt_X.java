82ea3fac1af46b79194160630c0c85bc828b44b6
WaltXin <xiongx@tcd.ie>
2017-07-08 20:10:50 +0100

    mid * mid may overlap, use divide

--- src/LeetCode/J69_Sqrt_X.java
+++ src/LeetCode/J69_Sqrt_X.java
@@ -9,20 +9,20 @@ import static org.junit.Assert.assertEquals;
  */
 public class J69_Sqrt_X {
     public int mySqrt(int x) {
-        int l = 0;
-        int r = x;
-        while (l <= r) {
+        if (x == 0) return 0;
+        int l = 1;
+        int r = (1 << 31) - 1;
+        while (true) {
             int mid = l + (r - l) / 2;
-            if (mid * mid > x) {
+            if (mid > x / mid) {
                 r = mid - 1;
             } else {
-                if ((mid + 1) * (mid + 1) > x) {
+                if ((mid + 1) > x / (mid + 1)) {
                     return mid;
                 }
                 l = mid + 1;
             }
         }
-        return l;
     }
     @Test
     public void test() {
