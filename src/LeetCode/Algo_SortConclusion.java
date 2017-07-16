bdb88e52037b2f47e3c027b8d8e19bebf867f86f
WaltXin <xiongx@tcd.ie>
2017-07-16 20:56:01 +0100

    Insert sort and optimise

--- src/LeetCode/Algo_SortConclusion.java
+++ src/LeetCode/Algo_SortConclusion.java
@@ -22,9 +22,22 @@ public class Algo_SortConclusion {
 		return arr;
 	}
 	
+	public int[] insertSort(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			int swapVal = arr[i];
+			int j;
+			for (j = i; j > 0 && arr[j - 1] > swapVal; j--) {
+				arr[j] = arr[j - 1];
+			}
+			arr[j] = swapVal;
+		}
+		return arr;
+	}
+	
 
     @Test
     public void test() {
         assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9}, new Algo_SortConclusion().selectionSort(new int[]{4,5,3,1,2,8,9,7,6}));
+        assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9}, new Algo_SortConclusion().insertSort(new int[]{4,5,3,1,2,8,9,7,6}));
     }
 }
