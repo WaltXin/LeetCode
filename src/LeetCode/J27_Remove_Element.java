57e844e2a1cf2183fb2aac64938792bfdfebaefe
WaltXin <xiongx@tcd.ie>
2018-07-02 21:47:24 +0100

    Addition change

--- src/LeetCode/J27_Remove_Element.java
+++ src/LeetCode/J27_Remove_Element.java
@@ -3,28 +3,25 @@ package LeetCode;
 public class J27_Remove_Element {
 
 	public static void main(String[] args) {
-		int[] arr = {3,2,2,3};
-		int[] res = removeElement(arr,3);
+		int[] arr = {0,1,2,2,3,0,4,2};
+		int[] res = removeElement(arr,2);
 		for (int i : res) {
 			System.out.println(i);
 		}
 	}
 	
 	 public static int[] removeElement(int[] nums, int val) {
-	        int index = 0;
+	        int offset = 0;
 	        for (int i = 0; i < nums.length; i++) {
 	            if (nums[i] == val) {
-	                index ++;
-	                continue;
-	            }
-	            int offset = i - index;
-	            int tem = nums[offset];
-	            nums[offset] = nums[i];
+	                offset ++;
+	            } 
+	            else {
+	            //swap nums[i] and nums[i - offset]
+	            int tem = nums[i - offset];
+	            nums[i - offset] = nums[i];
 	            nums[i] = tem;
-	        }
-	        
-	        if (index == nums.length) {
-	        	nums = null;
+	            }
 	        }
 	        return nums;
     }
